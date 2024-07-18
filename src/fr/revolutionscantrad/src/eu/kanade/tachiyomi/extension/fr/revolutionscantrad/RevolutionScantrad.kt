a1229da4ad846e97e20440bb3aaa89ae470a4fde
Mylloon <kennel.anri@tutanota.com>
2024-07-18 20:06:51 +0200

    init

---
+++ src/fr/revolutionscantrad/src/eu/kanade/tachiyomi/extension/fr/revolutionscantrad/RevolutionScantrad.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.fr.revolutionscantrad
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+class RevolutionScantrad : MangaThemesia("Revolution Scantrad", "https://revolutionscantrad.com", "fr")
